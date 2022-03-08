3465b385dc17c37630975f603213172ecbdbf610
ShaharMachluf <shahar6261@gmail.com>
2022-03-08 09:06:45 +0200

    new

---
+++ mat-a-main/mat.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "mat.hpp"
+
+using namespace std;
+
+namespace ariel{
+    string mat(int cols, int rows, char first, char second){
+        return ("ok");
+    }
+};
